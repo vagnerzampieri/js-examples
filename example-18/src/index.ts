25ee754836f080439dd28dfc603c3677579786be
Vagner Zampieri <vgzampieri@gmail.com>
2023-07-07 20:11:23 -0300

    add cors

--- example-18/src/index.ts
+++ example-18/src/index.ts
@@ -1,10 +1,17 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN
+};
+
+app.use(cors(corsOptions));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, world!');
