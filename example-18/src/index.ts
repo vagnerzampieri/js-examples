77483643b8eefa3b0fa96653fcb09401ae072ee5
Vagner Zampieri <vgzampieri@gmail.com>
2023-07-07 22:11:28 -0300

    remove comment

--- example-18/src/index.ts
+++ example-18/src/index.ts
@@ -95,10 +95,6 @@ app.use(
   })
 );
 
-// app.get('/', (req: Request, res: Response) => {
-//   res.send('Hello, world!');
-// });
-
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}/graphql`);
 });
