83d80b455a294e9600506974b17b1b515609f016
Vagner Zampieri <vgzampieri@gmail.com>
2023-07-08 21:11:30 -0300

    add helmet

--- example-18/src/app.ts
+++ example-18/src/app.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import helmet from 'helmet';
 import cors from 'cors';
 import { graphqlHTTP } from 'express-graphql';
 import { schema, root } from './schema';
@@ -12,6 +13,7 @@ const corsOptions: cors.CorsOptions = {
   origin: process.env.CORS_ORIGIN?.split(',') || [],
 };
 
+app.use(helmet());
 app.use(cors(corsOptions));
 
 app.use(
