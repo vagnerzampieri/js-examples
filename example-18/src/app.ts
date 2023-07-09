1deff890146bf13ad45db591290a3d77d1366bde
Vagner Zampieri <vgzampieri@gmail.com>
2023-07-08 22:16:53 -0300

    add others lybraries

--- example-18/src/app.ts
+++ example-18/src/app.ts
@@ -1,9 +1,7 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
 import cors from 'cors';
-import { graphqlHTTP } from 'express-graphql';
-import { schema, root } from './schema';
 
 dotenv.config();
 
@@ -15,14 +13,5 @@ const corsOptions: cors.CorsOptions = {
 
 app.use(helmet());
 app.use(cors(corsOptions));
-
-app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema,
-    rootValue: root,
-    graphiql: true,
-  })
-);
 
 export default app;
