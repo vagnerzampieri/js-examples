adb0ce9d3c0580110d10563fd3e3520c287d6c67
Vagner Zampieri <vgzampieri@gmail.com>
2023-07-31 16:28:21 -0300

    add manipulate DOM

--- example-19/src/add_user.js
+++ example-19/src/add_user.js
@@ -24,3 +24,61 @@ ul.lastElementChild.innerHTML = '<h1>Hello</h1>';
 
 const btn = document.querySelector('.btn');
 // btn.style.background = 'red';
+
+// events
+// click, mouseover, mouseout, mouseenter, mouseleave, mousemove
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log(e.target.className);
+  document.querySelector('#my-form').style.background = '#ccc';
+  document.querySelector('body').classList.add('bg-dark');
+  ul.lastElementChild.innerHTML = '<h1>Changed</h1>';
+});
+
+// keyboard events
+
+const nameInput = document.querySelector('#name');
+nameInput.addEventListener('input', (e) => {
+  document.querySelector('.container').append(nameInput.value);
+});
+
+// user form script
+
+// put DOM elements into variables
+
+const myForm = document.querySelector('#my-form');
+const nameInput2 = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+// listen for form submit
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  if (nameInput2.value === '' || emailInput.value === '') {
+    msg.classList.add('error');
+    msg.innerHTML = 'Please enter all fields';
+
+    // remove error after 3 seconds
+    setTimeout(() => msg.remove(), 3000);
+  } else {
+    // create new list item with user
+    const li = document.createElement('li');
+
+    // add text node with input values
+    li.appendChild(
+      document.createTextNode(`${nameInput2.value} : ${emailInput.value}`)
+    );
+
+    // append li to ul
+    userList.appendChild(li);
+
+    // clear fields
+    nameInput2.value = '';
+    emailInput.value = '';
+  }
+}
