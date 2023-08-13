63a269db4e9da0f50037beaf3775b7c26ca8edbe
Vagner Zampieri <vgzampieri@gmail.com>
2023-08-12 22:08:55 -0300

    start add js

---
+++ example-19/src/my_book_list.js
@@ -0,0 +1,28 @@
+// Book Class: Represents a Book
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+}
+
+// UI Class: Handle UI Tasks
+class UI {
+  static displayBooks() {
+    const StoredBooks = [
+      {
+        title: 'Book One',
+        author: 'John Doe',
+        isbn: '3434434'
+      },
+      {
+        title: 'Book Two',
+        author: 'Jane Doe',
+        isbn: '45545'
+      }
+    ];
+
+    const books = StoredBooks;
+  }
+}
