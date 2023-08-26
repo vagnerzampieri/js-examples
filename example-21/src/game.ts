49e169651d647c74bfa53de3e60b1ea096fb8fc2
Vagner Zampieri <vagner.zampieri@nuuvem.com.br>
2023-08-26 20:55:50 -0300

    web components

--- example-21/src/game.ts
+++ example-21/src/game.ts
@@ -1,7 +1,32 @@
+import './styles/main.scss'
+
+// https://github.com/juunegreiros/simmon-game
+
+const Pad = (value: Number) => {
+    const Pad = document.createElement('button')
+
+    Pad.classList.add('pad', `pad-${value}`)
+
+    return Pad
+}
+
+const Board = () => {
+    const Board = document.createElement('div')
+
+    Board.classList.add('board')
+    Board.append(Pad(1))
+    Board.append(Pad(2))
+    Board.append(Pad(3))
+    Board.append(Pad(4))
+
+    return Board
+}
+
 const Game = () => {
     const Game = document.createElement('div')
 
     Game.setAttribute('id', 'game')
+    Game.append(Board())
 
     return Game
 }
