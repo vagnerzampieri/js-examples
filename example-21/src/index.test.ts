4ec46d44a22f07df5604d6cbb51005e11b46802c
Vagner Zampieri <vagner.zampieri@nuuvem.com.br>
2023-08-26 21:48:36 -0300

    test

--- example-21/src/index.test.ts
+++ example-21/src/index.test.ts
@@ -1,8 +1,14 @@
 import { describe, it, expect } from 'vitest';
-import { sum } from './index';
+import { sum, subtract } from './index';
 
 describe('sum', () => {
   it('adds two numbers', () => {
     expect(sum(1, 2)).toBe(3);
+  });
+})
+
+describe('subtract', () => {
+  it('subtracts two numbers', () => {
+    expect(subtract(2, 1)).toBe(1);
   });
 })
