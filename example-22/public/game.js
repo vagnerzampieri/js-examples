cd637a481f51625abf06aa55d68b7f4c59ffb4b8
Vagner Zampieri <vagner.zampieri@nuuvem.com.br>
2023-09-29 15:39:16 -0300

    Add class Game

--- example-22/public/game.js
+++ example-22/public/game.js
@@ -17,56 +17,62 @@ const state = {
   loop: null
 }
 
-const shuffle = array => {
-  const clonedArray = [...array]
+class Game {
+  static config() {
+    return {
+      dimensions: 4,
+      emojis: ['💻', '🖥', '🎲', '🎮', '🖱', '🖲', '💽', '💾', '🕹', '☁️']
+    }
+  }
 
-  for (let index = clonedArray.length - 1; index > 0; index--) {
+  static renderCards() {
+    const { dimensions, emojis } = this.config()
+
+    const picks = this.pickRandom(emojis, (dimensions * dimensions) / 2)
+    const items = this.shuffle([...picks, ...picks])
+    const cards = `
+        <div class="board" style="grid-template-columns: repeat(${dimensions}, auto)">
+            ${items.map(item => `
+                <div class="card">
+                    <div class="card-front"></div>
+                    <div class="card-back">${item}</div>
+                </div>
+            `).join('')}
+       </div>
+    `
+
+    const parser = new DOMParser().parseFromString(cards, 'text/html')
+
+    El.board().replaceWith(parser.querySelector('.board'))
+  }
+
+  static shuffle(array) {
+    const clonedArray = [...array]
+
+    for (let index = clonedArray.length - 1; index > 0; index--) {
       const randomIndex = Math.floor(Math.random() * (index + 1))
       const original = clonedArray[index]
 
       clonedArray[index] = clonedArray[randomIndex]
       clonedArray[randomIndex] = original
+    }
+
+    return clonedArray
   }
 
-  return clonedArray
-}
+  static pickRandom(array, items) {
+    const clonedArray = [...array]
+    const randomPicks = []
 
-const pickRandom = (array, items) => {
-  const clonedArray = [...array]
-  const randomPicks = []
-
-  for (let index = 0; index < items; index++) {
+    for (let index = 0; index < items; index++) {
       const randomIndex = Math.floor(Math.random() * clonedArray.length)
 
       randomPicks.push(clonedArray[randomIndex])
       clonedArray.splice(randomIndex, 1)
+    }
+
+    return randomPicks
   }
-
-  return randomPicks
-}
-
-const generateGame = () => {
-  const dimensions = El.board().getAttribute('data-dimension')
-
-  if (dimensions % 2 !== 0) throw new Error("The dimension of the board must be an even number.");
-
-  const emojis = ['💻', '🖥', '🎲', '🎮', '🖱', '🖲', '💽', '💾', '🕹', '☁️']
-  const picks = pickRandom(emojis, (dimensions * dimensions) / 2)
-  const items = shuffle([...picks, ...picks])
-  const cards = `
-      <div class="board" style="grid-template-columns: repeat(${dimensions}, auto)">
-          ${items.map(item => `
-              <div class="card">
-                  <div class="card-front"></div>
-                  <div class="card-back">${item}</div>
-              </div>
-          `).join('')}
-     </div>
-  `
-
-  const parser = new DOMParser().parseFromString(cards, 'text/html')
-
-  El.board().replaceWith(parser.querySelector('.board'))
 }
 
 const timer = () => {
@@ -143,5 +149,5 @@ const attachEventListeners = () => {
   })
 }
 
-generateGame()
+Game.renderCards()
 attachEventListeners()
